a584a9004e6b10579453e20207216d401f779f61
Daan Gerits <daan.gerits@bigboards.io>
2013-12-02 14:09:53 +0100

    Added latest version

--- setup.sh
+++ setup.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-getopts "e:" EXTRA_ARGS
-if [ "$OPTARG" != "" ]; then
-  echo "Running with extra args: ${OPTARG}"
-  sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local -v -e "$OPTARG" prepare.yml
-else
-  sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local -v prepare.yml
-fi
+sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local $@ prepare.yml
