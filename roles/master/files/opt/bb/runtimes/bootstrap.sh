4a5fce1695f7950744ee77a527a3f0c8058683b0
Daan Gerits <daan.gerits@bigboards.io>
2014-05-06 20:27:28 +0200

    Made sure the new bootstrapper works
    Former-commit-id: 9b6af7ad5d01b901fa4bcaa1a44959bf5dd47f72

---
+++ roles/master/files/opt/bb/runtimes/bootstrap.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:rquillo/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local $@ setup.yml
+[ -z $1 ] && echo "No role has been given!" && exit 1
+[ -z $2 ] && echo "No sequence has been given!" && exit 1
+
+sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="role=${1} sequence=${2}"
