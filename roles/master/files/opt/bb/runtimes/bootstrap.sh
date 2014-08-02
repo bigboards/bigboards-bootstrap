90002d33b54f533bcba40bc05c66fbfde9d8d9c8
Daan Gerits <daan.gerits@bigboards.io>
2014-08-02 23:11:06 +0200

    Updated the bootstrapper for the new rootfs
    Former-commit-id: 12129f446490e7e03d60ad9f26ee17d5fd0667dd

--- roles/master/files/opt/bb/runtimes/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository -y ppa:rquillo/ansible
-sudo add-apt-repository -y "deb http://apt.bigboards.io/repo binary/"
-sudo apt-get update
-sudo apt-get -y --force-yes install ansible
-sudo apt-get -y --force-yes install bigboards-updater
