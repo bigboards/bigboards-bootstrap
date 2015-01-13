302c2d1d6f92d6bbc1cb8fe4fcf82cfc827bd381
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-01-13 12:00:39 +0100

    Adjust bootstrap setup.sh script to only require 1 parameter, nl. the sequence: the role is derived (1:=master, >=2:=slave)
    Former-commit-id: 1be46dbec9cb416facca006d4046a88f7268e27b

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-[ -z $1 ] && echo "No role has been given!" && exit 1
-[ -z $2 ] && echo "No sequence has been given!" && exit 1
+set -e
 
-sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="role=${1} sequence=${2}"
+[ -z $1 ] && echo "No sequence has been given!" && exit 1
+[[ ! $1 =~ ^-?[0-9]+$ ]] && echo "Sequence must be an integer!" && exit 1
+
+[[ $1 -eq 1 ]] && role="master"
+[[ $1 -gt 1 ]] && role="slave"
+
+[ -z $role ] && echo "No role was derived! Sequence must be an integer greater than 1" && exit 1
+#echo $role
+
+sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="role=${role} sequence=${1}"
