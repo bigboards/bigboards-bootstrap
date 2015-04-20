6f9e05d41cdaeca5104c37e97518aaac066b846a
Daan Gerits <daan@bigboards.io>
2015-04-20 17:20:18 +0200

    allowed a random number of nodes to be installed
    Former-commit-id: 6ac979d0ae384507ccfd2bac2963d3c9274c46ce

--- setup-all.sh
+++ setup-all.sh
@@ -2,33 +2,30 @@
 set -e
 
 [ -z $1 ] && echo "No hex name has been given!" && exit 1
+[ -z $1 ] && echo "No hex size has been given!" && exit 1
 
 NAME=$1
+SIZE=$2
+
 RANGE=`echo 10.$(shuf -i 0-255 -n 1).$(shuf -i 0-255 -n 1)`
 
 function install() {
 	sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="name=${1} range=${2} role=${3} sequence=${4}"
 }
 
-echo "Installing hex $NAME with ip range $RANGE"
+echo "Installing hex $NAME with $SIZE nodes and ip range $RANGE"
 
-read -p "Insert Storage for node 1 and press ENTER."
-install $NAME $RANGE master 1
+for i in $(seq 1 $SIZE); 
+do 
+	read -p "Insert Storage for node $i and press ENTER."
 
-read -p "Insert Storage for node 2 and press ENTER."
-install $NAME $RANGE slave 2
-
-read -p "Insert Storage for node 3 and press ENTER."
-install $NAME $RANGE slave 3
-
-read -p "Insert Storage for node 4 and press ENTER."
-install $NAME $RANGE slave 4
-
-read -p "Insert Storage for node 5 and press ENTER."
-install $NAME $RANGE slave 5
-
-read -p "Insert Storage for node 6 and press ENTER."
-install $NAME $RANGE slave 6
+	if [ "$i" -eq "1" ]; 
+	then
+		install $NAME $RANGE master $i
+	else 
+		install $NAME $RANGE slave $i
+	fi
+done
 
 echo "Done!"
 echo "The Hex named $NAME received internal ip address range ${RANGE}"
