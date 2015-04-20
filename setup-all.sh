fd155a7e3b62769c6472a49b19e425122d80502b
Daan Gerits <daan@bigboards.io>
2015-04-20 17:34:33 +0200

    passed the architecture thru the script to ansible
    Former-commit-id: 04155de895cd68c12424494484de112457e2d19b

--- setup-all.sh
+++ setup-all.sh
@@ -2,15 +2,19 @@
 set -e
 
 [ -z $1 ] && echo "No hex name has been given!" && exit 1
-[ -z $1 ] && echo "No hex size has been given!" && exit 1
+[ -z $2 ] && echo "No hex architecture has been given!" && exit 1
+[ -z $3 ] && echo "No hex size has been given!" && exit 1
 
 NAME=$1
+ARCH=$2
 SIZE=$2
 
 RANGE=`echo 10.$(shuf -i 0-255 -n 1).$(shuf -i 0-255 -n 1)`
 
+[[ "$ARCH" -ne "intel" ] && [ "$ARCH" -ne "arm" ]] && echo "Hex architecture should be 'intel' or 'arm'" && exit 1
+
 function install() {
-	sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="name=${1} range=${2} role=${3} sequence=${4}"
+	sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="name=${1} arch=${2} range=${3} role=${4} sequence=${5}"
 }
 
 echo "Installing hex $NAME with $SIZE nodes and ip range $RANGE"
@@ -21,9 +25,9 @@ do
 
 	if [ "$i" -eq "1" ]; 
 	then
-		install $NAME $RANGE master $i
+		install $NAME $ARCH $RANGE master $i
 	else 
-		install $NAME $RANGE slave $i
+		install $NAME $ARCH $RANGE slave $i
 	fi
 done
 
