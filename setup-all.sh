257b1419dc6284e0c0ad04eb886c02106d165a7d
calmera <daan@bigboards.io>
2015-05-18 10:32:46 +0200

    fixed some of the errors we encountered when creating images.
    Former-commit-id: 7493b8537ccd2b79a5711928cdd1fbf52e3ee637

--- setup-all.sh
+++ setup-all.sh
@@ -2,14 +2,34 @@
 set -e
 
 [ -z $1 ] && echo "No hex name has been given!" && exit 1
-[ -z $2 ] && echo "No hex architecture has been given!" && exit 1
-[ -z $3 ] && echo "No hex size has been given!" && exit 1
 
-NAME=$1
-ARCH=$2
-SIZE=$3
+function updateResumeFile() {
+cat > "/tmp/__bb_$1.resume" <<EOF
+NAME=$NAME
+ARCH=$ARCH
+SIZE=$SIZE
+OFFSET=$1
+RANGE=$RANGE
+EOF
+}
 
-RANGE=`echo 10.$(shuf -i 0-255 -n 1).$(shuf -i 0-255 -n 1)`
+
+if [[ "$2" == "--resume" ]];
+then
+	echo "resuming ... $1"
+	source "/tmp/__bb_$1.resume"
+else 
+	[ -z $2 ] && echo "No hex architecture has been given!" && exit 1
+	[ -z $3 ] && echo "No hex size has been given!" && exit 1	
+
+	NAME=$1
+	ARCH=$2
+	SIZE=$3
+	OFFSET=1
+	RANGE=`echo 10.$(shuf -i 0-255 -n 1).$(shuf -i 0-255 -n 1)`
+
+	updateResumeFile 1
+fi
 
 if [[ "$ARCH" != "intel" ]] && [[ "$ARCH" != "arm" ]]; 
 then 
@@ -19,11 +39,13 @@ fi
 
 function install() {
 	sudo ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i myhosts -c local setup.yml --extra-vars="name=${1} arch=${2} range=${3} role=${4} sequence=${5}"
+
+	[[ $? -eq 0 ]] && updateResumeFile $5
 }
 
 echo "Installing hex $NAME with $SIZE nodes and ip range $RANGE"
 
-for i in $(seq 1 $SIZE); 
+for i in $(seq $OFFSET $SIZE); 
 do 
 	read -p "Insert Storage for node $i and press ENTER."
 
